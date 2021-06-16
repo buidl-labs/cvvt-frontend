5d9ec3ae852fd509489a3d68c7f6531ea08605fb
Manan <gouhari21@hotmail.com>
2021-06-16 18:32:23 +0530

    implemented complete UI of Invest page

--- pages/app/invest.tsx
+++ pages/app/invest.tsx
@@ -17,7 +17,7 @@ function Invest() {
         <h1 className="text-2xl font-medium mb-10 text-gray-dark">
           Invest CELO
         </h1>
-        <main>
+        <main className="space-y-10">
           <div className="border border-gray-light rounded-md py-8 px-10">
             <h3 className="text-gray-dark text-xl">
               Step 1: Investment Amount
@@ -57,6 +57,77 @@ function Invest() {
             </div>
             <button className="bg-primary text-white text-lg block w-full rounded-md mt-5 py-3">
               Confirm &amp; Continue
+            </button>
+          </div>
+          <div className="border border-gray-light rounded-md py-8 px-10">
+            <h3 className="text-gray-dark text-xl">
+              Step 2: Vote For Validator
+            </h3>
+            <div className="text-gray mt-5">
+              <p className="font-medium">
+                Why am I voting? What are Validator Groups?
+              </p>
+              <p className="mt-3 max-w-5xl">
+                Its easier than it sounds. Here’s how it work:
+                <ul className="list-disc list-inside my-2 space-y-1">
+                  <li>
+                    The investment process of Celo is based on the mechanism
+                    where you vote for Validator Groups with your CELO
+                  </li>
+                  <li>
+                    When the Validator Groups you vote for performs well - you
+                    earn CELOs. Its that simple!
+                  </li>
+                </ul>
+                You don’t have to go through the hustle of deciding which
+                Validator Group to vote for. We have the most suited Group for
+                you. You can vote for it right-away!
+              </p>
+            </div>
+            <div className="mt-5 text-sm">
+              <div className="flex justify-between">
+                <span className="text-gray-dark">
+                  Recommended Validator Group to vote for:
+                </span>
+                <button className="text-primary">
+                  Edit Validator Group preference
+                </button>
+              </div>
+              <div className="border border-gray-light rounded-md grid grid-cols-5 gap-9 px-12 py-5 mt-3 text-center">
+                <div className="grid grid-rows-2 gap-2">
+                  <span className="text-gray">Name</span>
+                  <span className="text-gray-dark text-base">VG Name</span>
+                </div>
+                <div className="grid grid-rows-2 gap-2">
+                  <span className="text-gray">Group Score</span>
+                  <span className="text-gray-dark text-base">95.00%</span>
+                </div>
+                <div className="grid grid-rows-2 gap-2">
+                  <span className="text-gray">Performance Score</span>
+                  <span className="text-gray-dark text-base">96.34%</span>
+                </div>
+                <div className="grid grid-rows-2 gap-2">
+                  <span className="text-gray">Trust Score</span>
+                  <span className="text-gray-dark text-base">55.00%</span>
+                </div>
+                <div className="grid grid-rows-2 gap-2">
+                  <span className="text-gray">Estimated APY</span>
+                  <span className="text-gray-dark text-base">5.6%</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="border border-gray-light rounded-md py-8 px-10">
+            <h3 className="text-gray-dark text-xl">
+              Step 3: Activate Investment
+            </h3>
+            <p className="text-gray font-medium mt-5">Almost there!</p>
+            <p className="text-gray mt-3">
+              To finish your investment & start earning profits - please return
+              back in a day.
+            </p>
+            <button className="bg-primary text-white text-lg block w-full rounded-md mt-5 py-3">
+              Activate
             </button>
           </div>
         </main>
