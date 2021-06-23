bf62c396c3fea42738ff6b313621cddc4a8e2289
Manan <gouhari21@hotmail.com>
2021-06-24 02:38:56 +0530

    validator panel implemented in vg dashboard

--- components/vg/dash.tsx
+++ components/vg/dash.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import useVG from "../../hooks/useVG";
-import { ValidatorGroup } from "../../lib/types";
+import { Validator, ValidatorGroup } from "../../lib/types";
 import useStore from "../../store/vg-store";
 
 export default function VGDash() {
@@ -28,6 +28,7 @@ export default function VGDash() {
               <TransparencyScoreBar score={Number(VG?.TransparencyScore)} />
               <NavButtons />
               <PerformanceMetricsPanel VG={VG} />
+              <ValidatorsPanel VG={VG} />
             </>
           );
         }
@@ -88,7 +89,23 @@ function PerformanceMetricsPanel({ VG }: { VG: ValidatorGroup }) {
       <div className="grid grid-cols-4 mt-5 gap-28">
         <div className="grid grid-rows-2 gap-1 text-left">
           <p className="text-sm text-gray">Elected/Total Validators</p>
-          <p className="text-2xl font-medium">1/2</p>
+          <div className="text-2xl font-medium flex flex-wrap">
+            {VG.Validators.map((v) => (
+              <svg
+                className={`h-5 w-5 ml-2 mt-0.5 ${
+                  v.currently_elected ? "text-gray-dark" : "text-gray"
+                }`}
+                viewBox="0 0 32 32"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M31.9217 28.2182L25.8851 2.03636C25.53 0.872727 24.2102 0 23.5 0H8.5C7.61226 0 6.53233 0.872727 6.17724 2.03636L0.140599 28.2182C-0.392046 29.9636 0.673244 32 1.91608 32H29.9687C31.3891 32 32.2768 29.9636 31.9217 28.2182Z"
+                  fill="currentColor"
+                />
+              </svg>
+            ))}
+          </div>
         </div>
         <div className="grid grid-rows-2 gap-1 text-center">
           <p className="text-sm text-gray">Transparency Score</p>
@@ -138,6 +155,58 @@ function PerformanceMetricsPanel({ VG }: { VG: ValidatorGroup }) {
           </p>
         </div>
       </div>
+    </div>
+  );
+}
+
+function ValidatorsPanel({ VG }: { VG: ValidatorGroup }) {
+  return (
+    <div className="mt-10 border border-gray-light rounded-md p-10 text-gray-dark">
+      <div className="flex items-baseline justify-between">
+        <h4 className="text-xl font-medium">Affiliated Validators</h4>
+        <p className="font-medium">
+          Group Share: {(VG.GroupShare * 100).toFixed(0)}%
+        </p>
+      </div>
+      <div className="grid grid-cols-2 gap-5 mt-5">
+        {VG.Validators.map((v) => (
+          <ValidatorBlock validator={v} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function ValidatorBlock({ validator }: { validator: Validator }) {
+  return (
+    <div className="border border-gray-light rounded-md px-5 py-3">
+      <div className="flex items-baseline justify-between">
+        <h5 className="font-medium">
+          {validator.name ? validator.name : "Unknown Validator"}
+        </h5>
+        <p
+          className={`${
+            validator.currently_elected ? "text-gray-dark" : "text-gray"
+          } flex items-center`}
+        >
+          <svg
+            className={`h-4 w-4`}
+            viewBox="0 0 32 32"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M31.9217 28.2182L25.8851 2.03636C25.53 0.872727 24.2102 0 23.5 0H8.5C7.61226 0 6.53233 0.872727 6.17724 2.03636L0.140599 28.2182C-0.392046 29.9636 0.673244 32 1.91608 32H29.9687C31.3891 32 32.2768 29.9636 31.9217 28.2182Z"
+              fill="currentColor"
+            />
+          </svg>
+          <span className="ml-3 mt-0.5">
+            {validator.currently_elected ? "Elected" : "Refused"}
+          </span>
+        </p>
+      </div>
+
+      <p className="text-gray text-sm mt-2">{validator.address}</p>
     </div>
   );
 }
