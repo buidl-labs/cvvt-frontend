57d3349d5c8ce155c33ba78c01c6afe781cea6db
Manan <gouhari21@hotmail.com>
2021-06-26 20:56:13 +0530

    homepage feature section implemented

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
-import { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
 import { BigNumber } from "bignumber.js";
 import Nav from "../components/home/nav";
 import HeroIllustration from "../components/home/hero-illustration";
 import { fetchExchangeRate, fetchTargetAPY } from "../lib/utils";
+import FeatureIllustrationOne from "../components/home/feature-illustration-one";
+import FeatureIllustrationTwo from "../components/home/feature-illustration-two";
+import FeatureIllustrationThree from "../components/home/feature-illustration-three";
 
 export default function Home() {
   return (
     <div>
       <Nav />
-      <main className="mt-32">
+      <main className="my-32">
         <div className="px-36 grid grid-cols-2 gap-y-16">
           <div className="text-gray-dark">
             <div className="text-secondary-dark text-sm">
@@ -43,6 +46,43 @@ export default function Home() {
           </div>
           <div>
             <EarningCalculator />
+          </div>
+        </div>
+        <div className="mt-28 relative">
+          <div className="absolute top-0">
+            <svg
+              width="542"
+              height="254"
+              viewBox="0 0 542 254"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M-8.42555 201.127L10.9552 202.105C100.929 206.643 189.314 177.148 258.532 119.488V119.488V119.488C336.031 68.7295 426.12 40.526 518.728 38.03L525.489 37.8477"
+                stroke="#FDEABD"
+              />
+              <g clip-path="url(#clip0)">
+                <path
+                  d="M523.563 28.6451C519.834 27.5837 515.74 30.4919 514.419 35.1419C513.097 39.7926 515.051 44.4225 518.78 45.4832C522.51 46.5458 526.607 43.6365 527.926 38.9869C529.246 34.3354 527.293 29.7076 523.563 28.6451Z"
+                  fill="#FCDB8C"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0">
+                  <rect
+                    width="16.734"
+                    height="16.743"
+                    fill="white"
+                    transform="translate(511 31.0126) rotate(-14.2654)"
+                  />
+                </clipPath>
+              </defs>
+            </svg>
+          </div>
+          <div className="text-center">
+            <p className="text-gray text-sm">A simple and smart way of</p>
+            <p className="mt-5  text-3xl font-serif">Putting CELOs to Work</p>
+            <FeatureGrid />
           </div>
         </div>
       </main>
@@ -132,6 +172,54 @@ function EarningCalculator() {
           </div>
         </div>
       </div>
+    </div>
+  );
+}
+
+function FeatureGrid() {
+  const features = [
+    {
+      asset: <FeatureIllustrationOne />,
+      heading: "Start with 1 CELO",
+      subtext:
+        "Start earning profits with as low as 1 CELO in your wallet. The more you hold & invest, the more you earn.",
+    },
+    {
+      asset: <FeatureIllustrationTwo />,
+      heading: "Earn Automatically",
+      subtext:
+        "The CELOs earned in profit automatically gets added to your invested CELO for compounding.",
+    },
+    {
+      asset: <FeatureIllustrationThree />,
+      heading: "Earn without Risk",
+      subtext:
+        "When you invest CELO the invested CELO is never exposed to slashing, making it a risk free investment.",
+    },
+  ];
+  return (
+    <div className="grid grid-cols-3 gap-x-20 mt-16 px-56">
+      {features.map((f) => (
+        <FeatureItem asset={f.asset} heading={f.heading} subtext={f.subtext} />
+      ))}
+    </div>
+  );
+}
+
+function FeatureItem({
+  asset,
+  heading,
+  subtext,
+}: {
+  asset: JSX.Element;
+  heading: string;
+  subtext: string;
+}) {
+  return (
+    <div className="flex flex-col items-center">
+      {asset}
+      <h4 className="mt-10 text-xl font-medium">{heading}</h4>
+      <p className="mt-5 text-gray">{subtext}</p>
     </div>
   );
 }
