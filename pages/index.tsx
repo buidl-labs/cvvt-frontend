31e0ce2f827ce870e8c8438f578911376df3aa59
Manan <gouhari21@hotmail.com>
2021-06-27 22:09:25 +0530

    home page built till testimonial section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
-import React, { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
-import { BigNumber } from "bignumber.js";
+
 import Nav from "../components/home/nav";
-import HeroIllustration from "../components/home/hero-illustration";
-import { fetchExchangeRate, fetchTargetAPY } from "../lib/utils";
-import FeatureIllustrationOne from "../components/home/feature-illustration-one";
-import FeatureIllustrationTwo from "../components/home/feature-illustration-two";
-import FeatureIllustrationThree from "../components/home/feature-illustration-three";
+
+import HeroIllustration from "../components/home/illustrations/hero-illustration";
+import EarningCalculator from "../components/home/earning-calc";
+import FeatureGrid from "../components/home/feature-grid";
+
+import PiggyBank from "../components/home/illustrations/piggy-bank";
+import Website from "../components/home/illustrations/website";
+import Exchange from "../components/home/illustrations/exchange";
 
 export default function Home() {
   return (
     <div>
       <Nav />
       <main className="my-32">
-        <div className="px-36 grid grid-cols-2 gap-y-16">
+        <div className="px-36 grid grid-cols-2 gap-x-16">
           <div className="text-gray-dark">
             <div className="text-secondary-dark text-sm">
               Invest • Earn • Grow
@@ -85,141 +87,168 @@ export default function Home() {
             <FeatureGrid />
           </div>
         </div>
+        <div className="pl-36 pt-32 grid grid-cols-2 gap-x-16">
+          <div>
+            <p className="text-sm text-gray">
+              Welcome to the Future of Staking
+            </p>
+            <h4 className="font-serif text-dark-gray text-3xl mt-2">
+              Simple. Secure. Surreal.
+            </h4>
+            <p className="mt-3 text-gray max-w-xl">
+              Investing CELO is simple & easy, as it should be. We take pride in
+              binding this simplicity with utmost security by securing your
+              investment related transactions on the Celo blockchain.
+            </p>
+            <div className="mt-5">
+              <div className="flex items-center mt-5">
+                <div>
+                  <PiggyBank />
+                </div>
+                <p className="ml-5 text-gray">
+                  Simple, Quick & Delightful Investing Experience for You!
+                </p>
+              </div>
+              <div className="flex items-center mt-5">
+                <div>
+                  <Website />
+                </div>
+                <p className="ml-5 text-gray">
+                  Secure Investment based on Celo’s blockchain protocol.
+                </p>
+              </div>
+              <div className="flex items-center mt-5">
+                <div>
+                  <Exchange />
+                </div>
+                <p className="ml-5 text-gray">
+                  Withdraw the Invested CELO within 3 days (unlocking period).
+                </p>
+              </div>
+            </div>
+            <div className="mt-10 space-x-4 text-lg">
+              <Link href="/app/dashboard" passHref>
+                <a className="inline-block px-14 py-2 border-2 border-primary bg-primary text-white font-medium rounded-md shadow-md">
+                  Start Investing
+                </a>
+              </Link>
+              <Link href="/how" passHref>
+                <a className="inline-block px-14 py-2 bg-white text-primary border-2 border-primary font-medium rounded-md shadow-md">
+                  How it works?
+                </a>
+              </Link>
+            </div>
+          </div>
+          <div className="relative">
+            <img
+              className="absolute right-0"
+              src="/assets/home/home-dashboard.png"
+            />
+          </div>
+        </div>
+        <div className="mt-44">
+          <div className="text-center">
+            <p className="text-gray text-sm">Don't take our word for it</p>
+            <p className="mt-5  text-3xl font-serif">
+              See what other investors are saying
+            </p>
+          </div>
+          <div className="mt-12">
+            <TestimonialGrid />
+          </div>
+        </div>
       </main>
     </div>
   );
 }
 
-function EarningCalculator() {
-  const [exchangeRate, setExchangeRate] = useState(0);
-  const [estimatedAPY, setEstimatedAPY] = useState(new BigNumber(0));
-  const [celoAmountToInvest, setCeloAmountToInvest] = useState("1000.00");
-  const celoToInvestInUSD = useMemo(
-    () => parseFloat(celoAmountToInvest) * exchangeRate,
-    [celoAmountToInvest, exchangeRate]
-  );
-  const yearlyEarning = useMemo(() => {
-    if (celoAmountToInvest === "") return new BigNumber(0);
-    return new BigNumber(celoAmountToInvest).times(estimatedAPY).div(100);
-  }, [celoAmountToInvest, estimatedAPY]);
-  const yearlyEarningInUSD = useMemo(
-    () => yearlyEarning.times(exchangeRate),
-    [yearlyEarning, exchangeRate]
-  );
-
-  const monthlyEarning = useMemo(() => yearlyEarning.div(12), [yearlyEarning]);
-  const monthlyEarningInUSD = useMemo(
-    () => monthlyEarning.times(exchangeRate),
-    [monthlyEarning, exchangeRate]
-  );
-
-  useEffect(() => {
-    fetchExchangeRate().then((resp) => setExchangeRate(resp));
-    fetchTargetAPY().then((resp) =>
-      setEstimatedAPY(new BigNumber(parseFloat(resp.target_apy)))
-    );
-  }, []);
-  return (
-    <div className="p-10 text-gray-dark border border-gray-light rounded-md">
-      <h3 className="text-xl font-medium">Calculate Your Earnings</h3>
-      <div className="mt-5">
-        <label htmlFor="amount" className="block text-sm text-gray-dark">
-          If you invest this much CELO:
-        </label>
-
-        <div className="relative mt-2.5 w-full rounded-md shadow-sm text-left cursor-default focus:outline-none text-gray-dark-dark">
-          <input
-            type="number"
-            name="amount"
-            id="amount"
-            className="block w-full h-full px-5 py-4 text-xl bg-gray-light-light border border-gray-light rounded-md focus:border-primary focus:ring-primary focus:bg-primary-light-light"
-            placeholder="0.00 CELO"
-            value={celoAmountToInvest}
-            onChange={(e) => setCeloAmountToInvest(e.target.value)}
-          />
-          <div className="absolute inset-y-0 right-0 flex items-center">
-            <label htmlFor="currency" className="sr-only">
-              Currency
-            </label>
-            <div className="h-full py-0 pl-2 pr-7 text-lg rounded-md flex items-center justify-center text-primary">
-              <span>
-                $ {celoToInvestInUSD ? celoToInvestInUSD.toFixed(2) : "-"}
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="mt-10">
-        <p className="text-sm">You could be earning:</p>
-        <div className="flex items-center mt-3">
-          <p className="text-3xl text-primary font-medium">
-            {estimatedAPY ? `${estimatedAPY.toFixed(2)}%` : "-"} APY
-          </p>
-          <p className="text-gray ml-5">(Annual Percentage Yield)</p>
-        </div>
-        <div className="mt-5">
-          <p className="text-gray text-sm">Yearly Earning</p>
-          <div className="flex justify-between items-baseline mt-2">
-            <p>{yearlyEarning.toFixed(2)} CELO</p>
-            <p className="text-gray">$ {yearlyEarningInUSD.toFixed(2)}</p>
-          </div>
-        </div>
-        <div className="mt-5">
-          <p className="text-gray text-sm">Monthly Earning</p>
-          <div className="flex justify-between items-baseline mt-2">
-            <p>{monthlyEarning.toFixed(2)} CELO</p>
-            <p className="text-gray">${monthlyEarningInUSD.toFixed(2)}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function FeatureGrid() {
-  const features = [
+function TestimonialGrid() {
+  const testimonials = [
     {
-      asset: <FeatureIllustrationOne />,
-      heading: "Start with 1 CELO",
-      subtext:
-        "Start earning profits with as low as 1 CELO in your wallet. The more you hold & invest, the more you earn.",
+      avatar:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia dui eu gravida mollis. Cras nec scelerisque ligula. Nulla id aliquam dolor, vitae.",
+      name: "John DOe",
+      subtitle: "Software Developer",
     },
     {
-      asset: <FeatureIllustrationTwo />,
-      heading: "Earn Automatically",
-      subtext:
-        "The CELOs earned in profit automatically gets added to your invested CELO for compounding.",
+      avatar:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia dui eu gravida mollis. Cras nec scelerisque ligula. Nulla id aliquam dolor, vitae.",
+      name: "John DOe",
+      subtitle: "Software Developer",
     },
     {
-      asset: <FeatureIllustrationThree />,
-      heading: "Earn without Risk",
-      subtext:
-        "When you invest CELO the invested CELO is never exposed to slashing, making it a risk free investment.",
+      avatar:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia dui eu gravida mollis. Cras nec scelerisque ligula. Nulla id aliquam dolor, vitae.",
+      name: "John DOe",
+      subtitle: "Software Developer",
     },
   ];
+
   return (
-    <div className="grid grid-cols-3 gap-x-20 mt-16 px-56">
-      {features.map((f) => (
-        <FeatureItem asset={f.asset} heading={f.heading} subtext={f.subtext} />
+    <div className="grid grid-cols-3 gap-x-10 px-8">
+      {testimonials.map((t) => (
+        <TestimonialCard
+          avatar={t.avatar}
+          testimonial={t.text}
+          name={t.name}
+          subtitle={t.subtitle}
+        />
       ))}
     </div>
   );
 }
 
-function FeatureItem({
-  asset,
-  heading,
-  subtext,
+function TestimonialCard({
+  avatar,
+  testimonial,
+  name,
+  subtitle,
 }: {
-  asset: JSX.Element;
-  heading: string;
-  subtext: string;
+  avatar: string;
+  testimonial: string;
+  name: string;
+  subtitle: string;
 }) {
   return (
-    <div className="flex flex-col items-center">
-      {asset}
-      <h4 className="mt-10 text-xl font-medium">{heading}</h4>
-      <p className="mt-5 text-gray">{subtext}</p>
+    <div className="mx-auto bg-white border border-gray-light rounded-md p-10 relative overflow-hidden">
+      <div className="absolute top-0 left-0 z-0">
+        <svg
+          width="120"
+          height="240"
+          viewBox="0 0 120 240"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0 5.99999C0 2.68629 2.68629 0 6 0H120L87.1749 240H6C2.6863 240 0 237.314 0 234V5.99999Z"
+            fill="#D6F5E5"
+          />
+        </svg>
+      </div>
+      <div className="flex relative">
+        <div className="mr-6 flex flex-col justify-end">
+          <div className="rounded-full w-20 h-20 overflow-hidden -mb-4">
+            <img
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="max-w-sm text-gray">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+            distinctio ab quibusdam beatae aliquid a expedita suscipit nisi
+            totam tempora.
+          </div>
+          <div className="mt-5 space-y-3">
+            <p className="text-secondary">John Doe</p>
+            <p className="text-gray">Software Developer</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
