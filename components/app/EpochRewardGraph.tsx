1b4bc5f10cb7803b7dcf20ee0202fdba73df2e02
Manan <gouhari21@hotmail.com>
2021-07-16 14:05:35 +0530

    loading and empty states added to the graph

--- components/app/EpochRewardGraph.tsx
+++ components/app/EpochRewardGraph.tsx
@@ -14,6 +14,16 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
+import { Transition } from "@headlessui/react";
+import Loader from "react-loader-spinner";
+import Link from "next/link";
+
+enum STATES {
+  idle,
+  loading,
+  noRewards,
+  hasRewards,
+}
 
 const OPTIONS = ["7 days", "30 days", "All time"];
 function EpochRewardGraph({
@@ -28,17 +38,24 @@ function EpochRewardGraph({
     new Map<number, BigNumber>()
   );
   const [rewardsToShow, setRewardsToShow] = useState<any[]>([]);
+  const [status, setStatus] = useState<number>(STATES.idle);
 
   useEffect(() => {
+    setStatus(STATES.loading);
+
     fetchEpochRewards(kit, address).then((r) => {
       const rewardMap = r.reduce(
         (acc, val) => acc.set(val["epoch"], val["reward"]),
         new Map<number, BigNumber>()
       );
       setRewards(rewardMap);
+      setStatus(rewardMap.size > 0 ? STATES.hasRewards : STATES.noRewards);
     });
   }, []);
 
+  useEffect(() => {
+    console.log("status", status);
+  }, [status]);
   async function setDataForGraph() {
     const blockN = await kit.web3.eth.getBlockNumber();
     const epochNow = getEpochFromBlock(blockN, 17280);
@@ -72,7 +89,37 @@ function EpochRewardGraph({
   }, [rewards, selected]);
 
   return (
-    <div className="mt-10 px-10 py-8 border border-gray-light rounded-md">
+    <div className="mt-10 px-10 py-8 border border-gray-light rounded-md relative overflow-hidden">
+      <Transition
+        show={status !== STATES.hasRewards}
+        enter="transition-opacity duration-100"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-150"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="absolute z-20 inset-y-0 inset-x-0 bg-white  bg-opacity-90 flex items-center justify-center">
+          {status == STATES.loading && (
+            <div className="flex flex-col items-center space-y-4">
+              <Loader type="Puff" color="#35d07f" height={60} width={60} />
+              <h3 className="text-xl">Loading your past rewards</h3>
+            </div>
+          )}
+          {status == STATES.noRewards && (
+            <div className="flex flex-col items-center space-y-4">
+              <h3 className="text-xl">
+                Sorry, you've no past rewards to show yet 🥲
+              </h3>
+              <Link href="/app/invest" passHref>
+                <a className="bg-primary inline-block px-14 py-3 rounded-md text-white text-base shadow-sm hover:bg-primary-dark transition-all">
+                  Start Investing
+                </a>
+              </Link>
+            </div>
+          )}
+        </div>
+      </Transition>
       <Header selected={selected} setSelected={setSelected} />
       <RewardsGraph rewards={rewardsToShow} />
     </div>
@@ -105,7 +152,7 @@ export default EpochRewardGraph;
 
 const RewardsGraph = ({ rewards }: { rewards: any[] }) => (
   <div className="flex items-center justify-center mt-10">
-    <ResponsiveContainer width={"100%"} aspect={3}>
+    <ResponsiveContainer width={"100%"} aspect={4}>
       <LineChart
         data={rewards}
         margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
