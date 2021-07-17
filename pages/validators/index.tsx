aabe77b877cdb1908e8082c715ba86daaa0c7daa
Manan <gouhari21@hotmail.com>
2021-07-17 21:47:18 +0530

    sorting status toggle implemented; sorting logic remains.

--- pages/validators/index.tsx
+++ pages/validators/index.tsx
@@ -1,14 +1,87 @@
 import { Transition } from "@headlessui/react";
 import Link from "next/link";
-import React from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import { spawn } from "xstate";
 import Footer from "../../components/home/footer";
 import Nav from "../../components/home/nav";
 import useValidatorGroups from "../../hooks/useValidatorGroups";
 import { Validator, ValidatorGroup } from "../../lib/types";
 
 const formatter = new Intl.NumberFormat("en-US");
+
+enum Order {
+  ASC,
+  DESC,
+}
+
+type SortStatus = {
+  key: string;
+  order: Order;
+};
+
+const FIELDS = [
+  {
+    name: "Group Name",
+    key: "name",
+  },
+  {
+    name: "Elected/Total Validators",
+    key: "validators",
+  },
+  {
+    name: "Recieved Votes",
+    key: "recieved",
+  },
+  {
+    name: "Available Votes",
+    key: "available",
+  },
+  {
+    name: "Attestation Score",
+    key: "attestation",
+  },
+  {
+    name: "Overall Score",
+    key: "score",
+  },
+  {
+    name: "Estimated APY",
+    key: "apy",
+  },
+];
+
 function ValidatorExplorer() {
-  const { fetching, error, data: validatorGroups } = useValidatorGroups(true);
+  const [validatorGroups, setValidatorGroups] = useState([]);
+  const [sortStatus, setSortStatus] = useState<SortStatus>({
+    key: "score",
+    order: Order.DESC,
+  });
+
+  const {
+    fetching,
+    error,
+    data: validatorGroupsFromAPI,
+  } = useValidatorGroups(true);
+
+  useEffect(() => {
+    if (fetching || error) return;
+
+    if (validatorGroupsFromAPI?.ValidatorGroups.length > 0) {
+      setValidatorGroups(validatorGroupsFromAPI.ValidatorGroups);
+    }
+  }, [fetching, validatorGroupsFromAPI]);
+
+  const handleSort = (key: string) => {
+    if (sortStatus.key == key)
+      setSortStatus((currStatus) => ({
+        key,
+        order: currStatus.order == Order.ASC ? Order.DESC : Order.ASC,
+      }));
+    else setSortStatus({ key, order: Order.DESC });
+
+    console.log(sortStatus);
+  };
+
   return (
     <div className="text-gray-dark">
       <Nav />
@@ -16,13 +89,48 @@ function ValidatorExplorer() {
         <div className="px-40 py-10 border-b border-gray-light shadow">
           <h3 className="font-medium text-2xl">Validator Groups</h3>
           <div className="mt-8 px-9 grid grid-cols-7 font-medium text-sm text-gray text-center">
-            <p>Group Name</p>
-            <p>Elected/Total Validators</p>
-            <p>Recieved Votes</p>
-            <p>Available Votes</p>
-            <p>Attestation Score</p>
-            <p>Overall Score</p>
-            <p>Estimated APY</p>
+            {FIELDS.map((f) => (
+              <button
+                key={f.key}
+                onClick={() => handleSort(f.key)}
+                className={`hover:text-gray-dark focus:ring-2 focus:ring-primary focus:text-gray-dark transition-all rounded py-2 flex items-center justify-center ${
+                  sortStatus.key == f.key && "text-gray-dark"
+                }`}
+              >
+                <span>{f.name}</span>
+                {sortStatus.key == f.key && (
+                  <span className="ml-0.5">
+                    {sortStatus.order == Order.ASC ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-4 w-4"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-4 w-4"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    )}
+                  </span>
+                )}
+              </button>
+            ))}
           </div>
         </div>
         <Transition
@@ -40,8 +148,8 @@ function ValidatorExplorer() {
         </Transition>
 
         <div className="px-40 py-10 space-y-5 flex-1 min-h-screen">
-          {validatorGroups?.ValidatorGroups.map((VG: ValidatorGroup) => (
-            <Link href={`/validators/${VG.Address}`}>
+          {validatorGroups?.map((VG: ValidatorGroup) => (
+            <Link href={`/validators/${VG.Address}`} key={VG.Address}>
               <div className="grid grid-cols-7 text-center font-medium px-9 py-6 border border-gray-light rounded-md cursor-pointer hover:shadow-lg hover:-translate-y-0.5 hover:border-primary-light-light transform transition-all duration-100">
                 <div>{VG.Name ? VG.Name : "Unkown Group"}</div>
                 <div className="flex flex-wrap justify-center">
