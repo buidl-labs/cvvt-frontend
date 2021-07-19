eaab55addda0a68759e94e3ca5535e241aa18026
Manan <gouhari21@hotmail.com>
2021-07-19 23:59:41 +0530

    active link should be highlighted.

--- components/home/nav.tsx
+++ components/home/nav.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import ChurroFi from "../icons/churrofi";
 
 export default function Nav() {
+  const router = useRouter();
+  const currentPath = router.pathname;
   return (
     <nav className="bg-white fixed inset-x-0 top-0 z-40 flex justify-between items-center px-16 py-4 shadow-md">
       <Link href="/" passHref>
@@ -11,14 +14,18 @@ export default function Nav() {
         </a>
       </Link>
       <div className="flex items-center space-x-10">
-        <NavLink isButton={false} to="/validators">
+        <NavLink
+          isButton={false}
+          to="/validators"
+          active={currentPath == "/validators"}
+        >
           Validator Explorer
         </NavLink>
 
-        <NavLink isButton={false} to="/vg">
+        <NavLink isButton={false} to="/vg" active={currentPath == "/vg"}>
           For Validator Groups
         </NavLink>
-        <NavLink isButton={true} to="/app/dashboard">
+        <NavLink isButton={true} to="/app/dashboard" active={false}>
           Dashboard
         </NavLink>
       </div>
@@ -29,10 +36,12 @@ export default function Nav() {
 function NavLink({
   children,
   isButton,
+  active,
   to,
 }: {
   children: React.ReactChild;
   isButton: boolean;
+  active: boolean;
   to: string;
 }) {
   return (
@@ -40,7 +49,11 @@ function NavLink({
       <a
         className={`${
           !isButton
-            ? "text-gray text-lg hover:text-gray-dark hover:underline transition-all"
+            ? `text-lg hover:underline transition-all ${
+                active
+                  ? "text-primary hover:text-primary-dark"
+                  : "text-gray hover:text-gray-dark"
+              }`
             : "bg-primary px-14 py-3 rounded-md text-white text-base shadow-sm hover:bg-primary-dark transition-all"
         }`}
         href="#"
