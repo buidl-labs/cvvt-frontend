c29485cc6d105d94cb47a63f589a71ff62b8b43c
Manan <gouhari21@hotmail.com>
2021-08-11 22:03:22 +0530

    change copy when VG has completed their profile.

--- components/vg/transparency-score-bar.tsx
+++ components/vg/transparency-score-bar.tsx
@@ -1,8 +1,11 @@
 export default function TransparencyScoreBar({ score }: { score: number }) {
+  console.log(score);
   return (
     <div className="space-y-5 mt-6">
       <p className="text-gray-dark">
-        Complete your profile to increase your Transparency Score:{" "}
+        {score < 1
+          ? "Complete your profile to increase your Transparency Score:"
+          : "Congrats! You've completed your profile:"}{" "}
         <span className="ml-1 text-2xl font-medium">
           {(score * 100).toFixed(0)}/100%
         </span>
