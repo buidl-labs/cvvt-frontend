0ca0e8b2076a56f841023b4621a62fe48fff2e28
Manan Gouhari <gouhari21@hotmail.com>
2021-08-18 23:37:46 +0530

    added  button on Validator Profile

--- pages/validators/[address].tsx
+++ pages/validators/[address].tsx
@@ -6,6 +6,7 @@ import { ValidatorGroup } from "../../lib/types";
 import ProfileHeader from "../../components/vg/ProfileHeader";
 import PerformanceMetricsPanel from "../../components/vg/PerformanceMetricPanel";
 import ValidatorsPanel from "../../components/vg/ValidatorPanel";
+import Link from "next/link";
 
 function ValidatorGroupPage() {
   const address = useRouter().query.address;
@@ -23,10 +24,18 @@ function ValidatorGroupPage() {
     <>
       <Nav />
       {VG && (
-        <div className="px-40 mt-48 mb-24">
+        <div className="relative px-40 mt-48 mb-24">
           <ProfileHeader VG={VG} />
           <PerformanceMetricsPanel VG={VG} />
           <ValidatorsPanel VG={VG} />
+          <Link href={`/app/invest?vg=${VG.Address}`} passHref>
+            <a
+              target="_blank"
+              className="fixed bottom-20 right-40 bg-primary hover:bg-primary-dark focus:bg-primary-dark active:bg-primary-dark-dark focus:outline-none px-14 py-3 rounded-md text-white text-lg font-medium shadow-sm transition-all"
+            >
+              Invest in {VG.Name ? VG.Name : `${VG.Address.slice(0, 8)}...`}
+            </a>
+          </Link>
         </div>
       )}
     </>
