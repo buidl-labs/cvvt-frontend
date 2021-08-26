f8a339f983aa9c541400f43db33d6e2490e845b8
Manan Gouhari <gouhari21@hotmail.com>
2021-08-26 18:36:54 +0530

    about us page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Footer from "../components/home/footer";
 import Nav from "../components/home/nav";
-import { FaTwitter } from "react-icons/fa";
+import { FaTwitter, FaGlobe } from "react-icons/fa";
 
 function About() {
   const team = [
     {
-      name: "Jonh Doe",
-      title: "Product manager",
-      image:
-        "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+      name: "Saumay Agrawal",
+      title: "Project Lead",
+      image: "/public/assets/about/saumay.jpg",
       twitter: "https://twitter.com/ChurroFi",
+      website: "https://saumay.com/",
     },
     {
       name: "Jonh Doe",
@@ -44,25 +44,63 @@ function About() {
             Our Team
           </h2>
           <div className="grid grid-cols-1 lg:grid-cols-2 mt-16 mx-auto max-w-4xl gap-14">
-            {team.map((t) => (
-              <div key={t.name} className="flex justify-self-center">
-                <div className="mr-5">
-                  <img
-                    src={t.image}
-                    className="lg:w-24 lg:h-24 w-20 h-20 rounded-full overflow-hidden object-cover"
-                  />
-                </div>
-                <div className="text-left">
-                  <p className="text-gray-dark text-lg font-medium">{t.name}</p>
-                  <p className="text-sm text-gray mt-1">{t.title}</p>
-                  <div className="mt-2 text-secondary-light-light">
-                    <a href={t.twitter} target="_blank">
-                      <FaTwitter />
-                    </a>
-                  </div>
+            <div className="flex justify-self-center">
+              <div className="mr-5">
+                <img
+                  src={`/assets/about/saumay.jpg`}
+                  className="lg:w-24 lg:h-24 w-20 h-20 rounded-full overflow-hidden object-cover"
+                />
+              </div>
+              <div className="text-left">
+                <p className="text-gray-dark text-lg font-medium">
+                  Saumay Agarwal
+                </p>
+                <p className="text-sm text-gray mt-1">Project Lead</p>
+                <div className="mt-2 text-secondary-light-light">
+                  <a href="https://saumay.com/" target="_blank">
+                    <FaGlobe />
+                  </a>
                 </div>
               </div>
-            ))}
+            </div>
+            <div className="flex justify-self-center">
+              <div className="mr-5">
+                <img
+                  src={`/assets/about/prasang.jpg`}
+                  className="lg:w-24 lg:h-24 w-20 h-20 rounded-full overflow-hidden object-cover"
+                />
+              </div>
+              <div className="text-left">
+                <p className="text-gray-dark text-lg font-medium">
+                  Prasang Sharma
+                </p>
+                <p className="text-sm text-gray mt-1">Product Manager</p>
+                <div className="mt-2 text-secondary-light-light">
+                  <a href="https://twitter.com/prasang_7" target="_blank">
+                    <FaTwitter />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-self-center">
+              <div className="mr-5">
+                <img
+                  src={`/assets/about/manan.jpeg`}
+                  className="lg:w-24 lg:h-24 w-20 h-20 rounded-full overflow-hidden object-cover"
+                />
+              </div>
+              <div className="text-left">
+                <p className="text-gray-dark text-lg font-medium">
+                  Manan Gouhari
+                </p>
+                <p className="text-sm text-gray mt-1">Product Developer</p>
+                <div className="mt-2 text-secondary-light-light">
+                  <a href="https://twitter.com/manangouhari" target="_blank">
+                    <FaTwitter />
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
